cf7f29b748a48f8496b5f0d572031aab9311da16
ashu9999 <kmr.ashutosh9999@gmail.com>
2020-05-31 20:04:10 +0530

    fetching correct answers and responses

--- quizbee/src/index.js
+++ quizbee/src/index.js
@@ -6,7 +6,9 @@ import  QuestionBox from "./components/QuestionBox";
 
 class QuizBee extends Component{
   state={
-    questionBank: []
+    questionBank: [],
+    score: 0,
+    reponses: 0
   };
   getQuestions= () => {
     quizService().then(question =>{
@@ -15,6 +17,16 @@ class QuizBee extends Component{
       });
     });
   };
+  computeAnswer = (answer, correctAnswer) => {
+    if(answer === correctAnswer){
+      this.setState({
+        score: this.state.score + 1
+      });
+    }
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+    })
+  };
   componentDidMount(){
     this.getQuestions();
   }
@@ -22,12 +34,15 @@ class QuizBee extends Component{
     return(
       <div className="container">
         <div className="title">QuizBee</div>
-        {this.state.questionBank.length > 0 && this.state.questionBank.map(
+        {this.state.questionBank.length > 0 &&
+          this.state.responses < 5 &&
+          this.state.questionBank.map(
           ({question, answers, correct, questionId}) => (
             <QuestionBox
             question={question}
             options={answers}
             key={questionId}
+            selected={answer => this.computeAnswer(answer, correct)}
             />
           )
         )}
