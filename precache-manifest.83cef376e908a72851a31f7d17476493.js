46111a45773a034fc9228630229396a95b22c582
ashu9999 <kmr.ashutosh9999@gmail.com>
2020-06-01 11:33:53 +0530

    Updates

---
+++ precache-manifest.83cef376e908a72851a31f7d17476493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1e21337913e25e78e6309ae7afbf20",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "ceee92080c17bb5a5e74",
+    "url": "/react-quiz/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "63c2316b85f3801116bb",
+    "url": "/react-quiz/static/js/2.5f3dc2ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz/static/js/2.5f3dc2ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceee92080c17bb5a5e74",
+    "url": "/react-quiz/static/js/main.c358a4c8.chunk.js"
+  },
+  {
+    "revision": "1014d074e1fdd5827b53",
+    "url": "/react-quiz/static/js/runtime-main.e30798e7.js"
+  }
+]);
